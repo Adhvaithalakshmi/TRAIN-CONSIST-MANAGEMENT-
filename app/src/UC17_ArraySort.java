9a72d9017d32a91d46c25490e448e395f3761fdc
unknown <adhvaithalakshmi1@gmail.com>
2026-04-17 23:49:13 +0530

    UC17

---
+++ app/src/UC17_ArraySort.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class UC17_ArraySort{
+
+    public static void main(String[] args) {
+
+        System.out.println("=== Train Unit Sorting (Arrays.sort) ===\n");
+
+        // Step 1: Create array with different variable name
+        String[] coachTypes = {"Sleeper", "AC Chair", "First Class", "Cargo", "Guard"};
+
+        // Step 2: Before sorting
+        System.out.println("Before Sorting:");
+        System.out.println(Arrays.toString(coachTypes));
+
+        // Step 3: Sorting
+        Arrays.sort(coachTypes);
+
+        // Step 4: After sorting
+        System.out.println("\nAfter Sorting (Alphabetical Order):");
+        System.out.println(Arrays.toString(coachTypes));
+    }
+}
